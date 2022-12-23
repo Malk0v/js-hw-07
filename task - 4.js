3f228220d8eaa5642592d6987d068b5519f74847
Malk0v <106482398+Malk0v@users.noreply.github.com>
2022-12-23 21:51:45 +0200

    Update task - 4.js

--- task - 4.js
+++ task - 4.js
@@ -1,5 +1,4 @@
 // ## Задание 4
-
 // Счетчик состоит из спана и кнопок, которые должны увеличивать и уменьшать
 // значение счетчика на `1`.
 
@@ -9,10 +8,164 @@
 //   счетчика
 // - Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
 
-// ```html
-// <div id="counter">
-//   <button type="button" data-action="decrement">-1</button>
-//   <span id="value">0</span>
-//   <button type="button" data-action="increment">+1</button>
-// </div>
-// ```
+const onCounterValue = document.querySelector('#value');
+const decrementRef = document.querySelector('[data-action="decrement"]');
+const incrementRef = document.querySelector('[data-action="increment"]');
+const updateValue = document.querySelector('[data-action="updateValue"]');
+
+let counterValue = 0;
+
+const updateValueUi = () => {
+    onCounterValue.textContent = counterValue;
+}
+
+const increment = () => {
+    counterValue += 1;
+    updateValueUi();
+    console.log('click +');
+};
+
+const decrement = () => {
+    counterValue -= 1;
+    updateValueUi();
+    console.log('click -');
+};
+
+incrementRef.addEventListener('click', increment);
+decrementRef.addEventListener('click', decrement);
+
+
+// const onCounterValue = document.querySelector('#value');
+// const decrementRef = document.querySelector('[data-action="decrement"]');
+// const incrementRef = document.querySelector('[data-action="increment"]');
+// const updateValue = document.querySelector('[data-action="updateValue"]');
+
+// let counterValue = 0;
+
+// const increment = () => {
+//     counterValue += 1;
+//     console.log('click +');
+// };
+
+// const decrement = () => {
+//     counterValue -= 1;
+//     console.log('click -');
+// };
+
+// const updateValueUi = () => {
+//     onCounterValue.textContent = counterValue;
+// }
+
+// incrementRef.addEventListener('click', increment);
+// decrementRef.addEventListener('click', decrement);
+// updateValue.addEventListener('click', updateValueUi);
+
+
+
+// const CounterPlugin = function ({rootSelector,initialValue = 0,step = 1} = {}) {
+//     this._value = initialValue;
+//     this._step = step;
+
+//     this._refs = this._getRefs(rootSelector);
+
+//     this._bindEvents()
+//     this.updateValueUi()
+// };
+
+// CounterPlugin.prototype._getRefs = function (rootSelector) {
+//     const refs = {};
+//     refs.container = document.querySelector(rootSelector);
+//     refs.incrementBtn = refs.container.querySelector('[data-increment]');
+//     refs.decrementBtn = refs.container.querySelector('[data-decrement]');
+//     refs.value = refs.container.querySelector('[data-value]');
+
+//     return refs
+// };
+
+// CounterPlugin.prototype._bindEvents = function () {
+//     this._refs.incrementBtn.addEventListener('click', () => {
+//         console.log(this);
+//         this.increment();
+//         this.updateValueUi()
+//     })
+
+//     this._refs.decrementBtn.addEventListener('click', () => {
+//         console.log(this);
+//         this.decrement();
+//         this.updateValueUi()
+//     })
+// }
+
+// CounterPlugin.prototype.updateValueUi = function () {
+//     this._refs.value.textContent = this._value;
+// }
+
+// CounterPlugin.prototype.increment = function () {
+//     this._value += this._step;
+// };
+
+// CounterPlugin.prototype.decrement = function () {
+//     this._value -= this._step;
+// };
+
+// const counter1 = new CounterPlugin({ rootSelector: '#counter-1', initialValue:10, step: 10 });
+// console.log('counter1', counter1);
+
+// const counter2 = new CounterPlugin({ rootSelector: '#counter-2', initialValue:8, step: 1 });
+// console.log('counter2', counter2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//============================================
+
+
+// function incrementClick() {
+//     updateDisplay(++counterValue);
+// }
+
+// function resetCounter() {
+//     counterValue = 1;
+//     updateDisplay(val);
+// }
+
+// function updateDisplay(val) {
+//     document.getElementById("onCounterValue").textContent = val;
+// }
+
+// console.log(counterValue);
+
+// //======
+
+// function increment() {
+//     counterValue += step;
+// };
+
+// incrementRef.addEventListener('click', () => {
+//     (counterValue ++);
+// });
+
+// decrementRef.addEventListener('click', () => {
+//     counterValue++;
+// });
+
+// console.log(counterValue);
+
+// const updateValueUi = function () {
+//     onCounterValue.textContent = current.value;
+// }
